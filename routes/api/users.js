e7a32c3d82f8cb79413fd56f2a8ceaf4640fb256
Mykola Berezhniuk <bermukola@gmail.com>
2022-12-27 17:29:47 +0200

    add update Subscription method

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { ctrlWrapper, auth } = require('../../middlewares');
+const { ctrlWrapper, auth, validation } = require('../../middlewares');
 const { users: ctrl } = require('../../controllers');
+const { userSchema } = require('../../schemas');
 
 router.get('/current', auth, ctrlWrapper(ctrl.getCurrentUser));
+
+router.patch(
+  '/subscription',
+  auth,
+  validation(userSchema.updateUserSubscriptionSchema),
+  ctrlWrapper(ctrl.updateSubscription)
+);
 
 module.exports = router;
