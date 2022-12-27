357d4cc4820c95d7bbd528d9343dab8ce00f17ef
Mykola Berezhniuk <bermukola@gmail.com>
2022-12-27 13:10:34 +0200

    add logic for logout

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,5 +1,17 @@
+const { User } = require('../../models');
+
 const logoutUser = async (req, res) => {
-  
-}
+  // NOTE: Here logout implement on clint side because JWT state
+  // less and does not have method for deactivate token.
+  // NOTE: For deactivate token you can use JWT-REDIS library.
+
+  await User.findOneAndUpdate({ _id: req.user._id }, { token: '' });
+
+  res.status(204).json({
+    status: 'success',
+    code: 204,
+    message: 'User successfully logout',
+  });
+};
 
 module.exports = logoutUser;
