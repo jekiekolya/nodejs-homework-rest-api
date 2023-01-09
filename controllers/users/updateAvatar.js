6252de9b4cfeeb246724bd5ff4eef42cbab6af79
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-09 23:20:22 +0200

    all main without jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -6,12 +6,22 @@ const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}`;
+
   try {
-    const resultUpload = path.join(avatarsDir, originalname);
+    const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join('public', 'avatars', originalname);
+    const avatarURL = path.join('avatars', imageName);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
-    res.json({ avatarURL });
+
+    res.json({
+      status: 'success',
+      code: 200,
+      data: {
+        avatarURL,
+      },
+    });
   } catch (error) {
     await fs.unlink(tempUpload);
     throw error;
