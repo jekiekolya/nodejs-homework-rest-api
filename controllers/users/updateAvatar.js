58540e9b9bdd9fc1ff689f7ba20fe7e72582c16d
Mykola Berezhniuk <bermukola@gmail.com>
2023-01-09 22:55:16 +0200

    add logic for update avatar

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -11,6 +11,7 @@ const updateAvatar = async (req, res) => {
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join('public', 'avatars', originalname);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
+    res.json({ avatarURL });
   } catch (error) {
     await fs.unlink(tempUpload);
     throw error;
